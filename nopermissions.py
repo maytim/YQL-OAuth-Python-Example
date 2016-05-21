74813c680bcc15b0fec5a31201892f6a5eeee263
Tim Maytom <timothy.maytom@gmail.com>
2016-05-20 22:16:32 -0700

    Update nopermissions.py

--- nopermissions.py
+++ nopermissions.py
@@ -1,21 +1,20 @@
 #libraries for OAuth
 import requests
-from requests import OAuth1
-
-#library to parse JSON
-import json
+from requests_oauthlib import OAuth1
 
 #keys
 client_key = '...enter your unique client key...'
 client_secret = '...enter your unique client secret...'
 
 #Using OAuth1 to make a simple query request on when your application doesn't require any permissions
-query_url = 'http:'
+query_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
 queryoauth = OAuth1(client_key, client_secret, signature_type='query')
 r = requests.get(url=query_url, auth=queryoauth)
 
 print(r.content)
 
-#then to parse the json result
+#library to parse JSON
+import json
+
 result = json.loads(r.content.decode('utf8'))
 print(result)
